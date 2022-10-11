0d0d91d3e667ce59d6e68e4a1c7d70d1684c1315
Wesley Eugênio <wesleyeugenio3@gmail.com>
2022-10-11 10:37:30 -0300

    header feito e página de ofertas em andamento

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,19 @@
 import "./styles.css";
+import Logo from "../../assets/logo-negative.svg";
 
-const Header = ({}) => {
-  return <header className='header'></header>;
+const Header = () => {
+  return (
+    <header className='header'>
+      <nav className='header__nav'>
+        <img className='header__logo' src={Logo} alt='Logo' />
+        <ul className='header__group-links'>
+          <li className='header__link'>Serviços</li>
+          <li className='header__link'>Sobre</li>
+          <li className='header__link'>Contato</li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
