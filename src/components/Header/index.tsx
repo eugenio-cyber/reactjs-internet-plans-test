b525598751024f885cb9f376a23910abe4c06238
Wesley Eugênio <wesleyeugenio3@gmail.com>
2022-10-11 15:13:04 -0300

    adição de responsividade ao header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,36 @@
 import "./styles.css";
 import Logo from "../../assets/logo-negative.svg";
+import Menu from "../../assets/menu-hamburger.png";
+import { useState } from "react";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickMenu = () => {
+    setOpen(!open);
+  };
+
   return (
     <header className='header'>
-      <nav className='header__nav'>
-        <img className='header__logo' src={Logo} alt='Logo' />
-        <ul className='header__group-links'>
-          <li className='header__link'>Serviços</li>
-          <li className='header__link'>Sobre</li>
-          <li className='header__link'>Contato</li>
+      <nav className={`header__nav ${open && "fullscreen-menu"}`}>
+        <img className='header__logo' src={Logo} alt='logo' />
+        <ul className={"header__group-links"}>
+          <li className='header__link' onClick={() => handleClickMenu()}>
+            Serviços
+          </li>
+          <li className='header__link' onClick={() => handleClickMenu()}>
+            Sobre
+          </li>
+          <li className='header__link' onClick={() => handleClickMenu()}>
+            Contato
+          </li>
         </ul>
+        <img
+          className='header__menu'
+          src={Menu}
+          alt='menu'
+          onClick={() => handleClickMenu()}
+        />
       </nav>
     </header>
   );
